c7f24de86e4676091c3d9277781ea15ab16d9d52
Oleksandr Burko <sashaburko000@gmail.com>
2019-02-14 14:35:51 +0200

    add Data and Core projects to solution, create db

---
+++ e-folio.core/Entities/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace e_folio.core.Entities
+{
+    public class Project
+    {
+        public string Id { get; set; }
+        public string NameProject { get; set; }
+        public string NameOrder { get; set; }
+    }
+}
---
+++ e-folio.data/eFolioDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO.Compression;
+using Microsoft.EntityFrameworkCore;
+using e_folio.core.Entities;
+using e_folio.core;
+
+namespace e_folio.data
+{
+    public class eFolioDBContext : DbContext
+    {
+        public eFolioDBContext() : base(new DbContextOptionsBuilder().UseSqlServer("Data Source=.;Initial Catalog=eFolio;").Options)
+        {
+                
+        }
+
+        public DbSet<Project> Projects { get; set; }   
+    }
+}
