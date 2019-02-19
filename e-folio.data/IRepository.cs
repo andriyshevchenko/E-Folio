711a6e267f66b376d87d007904a007f6ddd768e5
OstapRoik <33757680+OstapRoik@users.noreply.github.com>
2019-02-19 12:29:48 +0200

    Initial version

---
+++ e-folio.data/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using e_folio.core.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace e_folio.data
+{
+    interface IRepository : IDisposable
+    {
+        IEnumerable<Project> GetProjectList();
+        IEnumerable<Project> Search(string request);
+        Project GetProject(int id);
+        void Create(Project project);
+        void Update(Project project);
+        void Delete(int id);
+        void Save();
+    }
+}
