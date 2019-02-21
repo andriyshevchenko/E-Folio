0c2a3386519ca31695fb2f624f8d28a40ee16253
yuriilevkopz <yuriilevko99@gmail.com>
2019-02-21 15:18:26 +0200

    Add Update in elasticsearch

--- e-folio.data/ElasticSearch/IEfolioElastic.cs
+++ e-folio.data/ElasticSearch/IEfolioElastic.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
-namespace eFolio.Elastic
+namespace eFolio.Elastic;
 {
     public interface IEfolioElastic
     {
         void AddItem(ElasticProjectData item);
         void AddItem(string path);
         List<ElasticProjectData> SearchItems(string searchString);
+        void DeleteItem(int _Id);
+        ElasticProjectData GetItemById(int _Id);
     }
 }
