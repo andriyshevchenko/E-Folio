ef406ec1e64623620d459585b83451428b592fe3
Shevchenko Andriy <shewchenkoandriy@gmail.com>
2019-02-27 23:57:36 +0200

    Add .UpdateId methods to Project and Developer

--- eFolio.DTO/Common/Developer.cs
+++ eFolio.DTO/Common/Developer.cs
@@ -4,12 +4,24 @@ namespace eFolio.DTO
 {
     public class Developer
     {
+        public Developer()
+        {
+
+        }
         public Developer(int id, string fullName, string cVLink)
         {
             Id = id;
             FullName = fullName;
             CVLink = cVLink;
             Projects = new List<Project>();
+        }
+
+        public void Update(int id)
+        {
+            if (id > 0)
+            {
+                Id = id; 
+            }
         }
 
         public int Id { get; set; }
