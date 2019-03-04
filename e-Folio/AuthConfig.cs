f8950aace064d857445746ae8a3eec357c21ce11
Oleksandr Burko <sashaburko000@gmail.com>
2019-03-04 16:01:34 +0200

    add AuthConfig

--- e-Folio/AuthConfig.cs
+++ e-Folio/AuthConfig.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+// using IdentityServer4.Models;
 
 namespace eFolio.API
 {
     public class AuthConfig
     {
+       // public static IEnumerable<IdentityResource> 
     }
 }
