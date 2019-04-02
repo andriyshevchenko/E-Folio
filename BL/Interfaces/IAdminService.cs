a14ee0a6a959cbd6bfb27afd41a59cacbad9ba31
yuriilevkopz <yuriilevko99@gmail.com>
2019-04-02 13:00:35 +0300

    Admin Service Admin Repository

---
+++ BL/Interfaces/IAdminService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eFolio.BL.Interfaces
+{
+    class IAdminService
+    {
+    }
+}
---
+++ BL/Services/AdminService.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using eFolio.BL.Interfaces;
+using eFolio.BL.Repositories;
+using eFolio.EF;
+
+namespace eFolio.BL.Services
+{
+    class AdminService : IAdminService
+    {
+        private AdminRepository adminRepository;
+        public AdminService(AuthDBContext authDB)
+        {
+            adminRepository = new AdminRepository(authDB);
+        }
+        public void Delete(int id)
+        {
+            adminRepository.Delete(id);
+        }
+        public void Update(UserEntity user)
+        {
+            adminRepository.Update(user);
+        }
+        public UserEntity GetUser(int id)
+        {
+            var userEntity = adminRepository.GetUser(id);
+            return userEntity;
+        }
+        public IEnumerable<UserEntity> GetUsersList()
+        {
+            return adminRepository.GetUsersList();
+        }
+    }
+}
