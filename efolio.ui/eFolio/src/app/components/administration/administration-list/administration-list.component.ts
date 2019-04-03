94a664c411f76e6f0aa41b26bf78648eeb2593c3
OstapRoik <33757680+OstapRoik@users.noreply.github.com>
2019-04-03 15:31:06 +0300

    Rewritted admin-page

--- efolio.ui/eFolio/src/app/components/administration/administration-list/administration-list.component.ts
+++ efolio.ui/eFolio/src/app/components/administration/administration-list/administration-list.component.ts
@@ -1,33 +1,47 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component } from '@angular/core';
 import { AdministrationService } from 'src/app/services/administration.service';
 import { User } from 'src/app/components/models/user.model';
 import { MatTableDataSource } from '@angular/material';
+
 @Component({
   selector: 'app-administration-list',
   templateUrl: './administration-list.component.html',
   styleUrls: ['./administration-list.component.scss']
 })
-export class AdministrationListComponent implements OnInit {
-  // @Output() public showSpinner = new EventEmitter<any>();
+export class AdministrationListComponent {
+  public showUsersTable: boolean = false;
+  public showProjectsTable: boolean = false;
+  public showDevelopersTable: boolean = false;
+  public users: User[] = [];
+  public displayedColumns: string[] = ['id', 'firstName', 'lastName', 'userName', 'email', 'emailConfirmed'];
+  public dataSource = new MatTableDataSource();
 
-  users: User[] = [];
-
-  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'userName', 'email', 'emailConfirmed'];
-  dataSource = new MatTableDataSource();
   constructor(private administrationService: AdministrationService) { }
 
-  ngOnInit() {
-  }
-
   showAllUsers() {
+    this.showUsersTable = true;
+    this.showProjectsTable = false;
+    this.showDevelopersTable = false;
+    this.users = [];
     this.administrationService.getAllUsers()
       .subscribe(responce => {
         this.getData(responce);
         this.dataSource.data = this.users;
-        console.log(responce);
       },
         (error) => console.log(error)
       );
+  }
+
+  showAllProjects() {
+    this.showUsersTable = false;
+    this.showProjectsTable = true;
+    this.showDevelopersTable = false;
+  }
+
+  showAllDevelopers() {
+    this.showUsersTable = false;
+    this.showProjectsTable = false;
+    this.showDevelopersTable = true;
   }
 
   getData(responce) {
